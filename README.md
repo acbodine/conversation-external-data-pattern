bca3dc959d32dece27455f18fbba840c87ca6494
Andrew C. Bodine <acbodine@us.ibm.com>
2017-06-23 12:30:53 -0400

    Adds most basic Express, TravisCI, and Mocha.js artifacts. Implements basic Express application for serving enhanced Watson Conversations.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # conversation-external-data-pattern
 Demonstrates a best practice pattern for integrating external data sources into Watson Conversation workflows.
+
+### Setup
+TODO
+
+### Test
+TODO
+
+### Run
+TODO
+
+### Contribute
+TODO
---
+++ index.js
@@ -0,0 +1,20 @@
+var
+    cfenv   = require('cfenv'),
+    express = require('express')
+;
+
+var port = 80;
+
+// If we aren't running in 'development' mode, then we expect to be running
+// inside a CloudFoundry env, so get the assigned port from the platform.
+if (process.env.NODE_ENV !== 'development') port = cfenv.getAppEnv().port;
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send("Hello from conversation-external-data-pattern application.");
+});
+
+app.listen(port, function () {
+    console.log('Ready to handle conversations @ localhost:', port);
+});
